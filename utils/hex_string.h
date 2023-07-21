434e03afddaacf51740367ea7e60905e4f9cefa1
Andrii Demchenko <demch.andrew@gmail.com>
2023-07-22 01:49:43 +0200

    Added hex string class with the utility functions

---
+++ utils/hex_string.h
@@ -0,0 +1,26 @@
+#ifndef HEX_STRING_H
+#define HEX_STRING_H
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+class HexString {
+public:
+    static HexString fromBuffer(const std::vector<uint8_t>& buffer);
+    static HexString fromUint8Array(const std::vector<uint8_t>& arr);
+    static HexString ensure(const std::string& hexString);
+    explicit HexString(const std::string& hexString);
+
+    std::string hex() const;
+    std::string noPrefix() const;
+    std::string toShortString() const;
+    std::vector<uint8_t> toUint8Array() const;
+
+    // Add other methods and members as needed
+
+private:
+    std::string hexString;
+};
+
+#endif  // HEXSTRING_H
